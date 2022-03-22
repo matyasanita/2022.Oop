53bee2cefb2687f0cf41a36c816179dcfe91a610
Anita <matays.anita@ms.student.sapientia.ro>
2022-03-22 23:04:17 +0200

    ¯\_( ͡❛ ͜ʖ ͡❛)_/¯

--- labor-04/src/oop/labor04/lab4_3/MyArray.java
+++ labor-04/src/oop/labor04/lab4_3/MyArray.java
@@ -2,43 +2,57 @@ package oop.labor04.lab4_3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
+import static java.lang.Math.pow;
+
 public class MyArray {
-    private static int length;
-    private static double[] elements;
+    private int length;
+    private double[] elements;
 
     public MyArray(int length){
         this.length = length;
+        elements = new double[length];
 
     }
     //public MyArray(MyArray) //itt at kell masolni kezzel, nem memoriacimet adok at
-    public MyArray(double[] elements){
-        for(double obj : elements){
-            this.elements[length] = obj;
-            length++;
+    public MyArray(double[] array){
+        this.length = array.length;
+        this.elements = new double[length];
+
+        int i =0;
+
+        for( double e : array ){
+            this.elements[i] = e;
+            i++;
         }
     }
 
     public MyArray(MyArray array){
         this.length = array.getLength();
+        this.elements = new double[length];
+
         int i= 0;
-        for(double obj : array.getElements()){
-            this.elements[i] = obj;
+        for(double e : array.getElements()){
+            this.elements[i] = e;
             i++;
         }
     }
 
-    public MyArray (File fileName){
+    public MyArray (String fileName){
         try {
-            Scanner sc = new Scanner(new File(String.valueOf(fileName)));
-            length = Integer.parseInt(sc.nextLine());
+            Scanner sc = new Scanner(new File(fileName));
+            this.length = Integer.parseInt(sc.nextLine());
+            this.elements = new double[length];
+
             String line = sc.nextLine();
             String[] arrayElements = line.split(" ");
             int index = 0;
-            for (String obj : arrayElements){
-                elements[index] = Integer.parseInt(obj);
+            for (String s : arrayElements){
+                this.elements[index] = Integer.parseInt(s);
+                index ++;
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -46,11 +60,11 @@ public class MyArray {
     }
 
     //getters
-    public static double[] getElements() {
+    public double[] getElements() {
         return elements;
     }
 
-    public static int getLength() {
+    public int getLength() {
         return length;
     }
 
@@ -58,7 +72,41 @@ public class MyArray {
     public void fillRandom(double a, double b){
         for(int i=0; i<length; i++){
             Random rand = new Random();
-            elements[i] = rand.nextDouble((b-a) + 1);
+            elements[i] = rand.nextDouble((a) + b-a);
         }
+    }
+
+    public double mean(){
+        double m = 0.0;
+
+        for (double e : elements) {
+            m += e;
+        }
+
+        return m/length;
+    }
+
+    public double stddev(){
+        double m = this.mean();
+        double standDev = 0.0;
+
+        for( double num : elements ){
+            standDev += pow((num - m), 2);
+        }
+
+        double res = standDev / length;
+        res = Math.sqrt(res);
+        return res;
+    }
+
+    public void sort(){
+        Arrays.sort(elements);
+    }
+
+    public void print(String s){
+        String res = Arrays.toString(elements);
+        res = res.replace("["," ");
+        res = res.replace("]"," ");
+        System.out.println(s +": " + res);
     }
 }
